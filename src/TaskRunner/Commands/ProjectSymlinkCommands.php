56564972f581cc1846537037e529e841e986abd2
Antonio De Marco <antonio@nuvole.org>
2021-04-12 12:07:52 +0200

    EWPP-964: Setup tests.

--- src/TaskRunner/Commands/ProjectSymlinkCommands.php
+++ src/TaskRunner/Commands/ProjectSymlinkCommands.php
@@ -7,7 +7,6 @@ namespace OpenEuropa\DrupalProjectSymlink\TaskRunner\Commands;
 use OpenEuropa\TaskRunner\Commands\AbstractCommands;
 use OpenEuropa\TaskRunner\Contract\ComposerAwareInterface;
 use OpenEuropa\TaskRunner\Traits\ComposerAwareTrait;
-use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Project symlink commands.
@@ -23,5 +22,35 @@ class ProjectSymlinkCommands extends AbstractCommands implements ComposerAwareIn
      */
     public function symlink()
     {
+        $drupalRoot = $this->getConfig()->get("drupal.root");
+        $projectDirectory = $drupalRoot.DIRECTORY_SEPARATOR.$this->getProjectTypeDirectory().DIRECTORY_SEPARATOR.$this->composer->getProject();
+
+        $tasks = [
+            // Make sure we do not have a release directory yet.
+            $this->taskFilesystemStack()->remove([$projectDirectory]),
+        ];
+
+        return $this->collectionBuilder()->addTaskList($tasks);
+    }
+
+    /**
+     * Return current project type base directory.
+     *
+     * @return string
+     *   Base directory name, either "modules", "themes" or "profiles".
+     */
+    protected function getProjectTypeDirectory()
+    {
+        $mapping = [
+            'drupal-module' => 'modules',
+            'drupal-theme' => 'themes',
+            'drupal-profile' => 'profile',
+        ];
+        $projectType = $this->composer->getType();
+        if (!array_key_exists($projectType, $mapping)) {
+            throw new \RuntimeException("Missing or not supported Drupal project type in composer.json. Supported types: 'drupal-module', 'drupal-theme' and 'drupal-profile'.");
+        }
+
+        return $mapping[$projectType];
     }
 }
---
+++ tests/Commands/ProjectSymlinkCommandsTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace OpenEuropa\DrupalProjectSymlink\Tests\Commands;
+
+use OpenEuropa\TaskRunner\Services\Composer;
+use OpenEuropa\TaskRunner\TaskRunner;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test project symlink command.
+ */
+class ProjectSymlinkCommandsTest extends TestCase
+{
+
+    /**
+     * Test project symlink command.
+     *
+     * @param array $configuration
+     *   Task runner configuration, it will be dumped in a runner.yml file.
+     * @param array $project
+     *   Array describing the test project, as in: its name, type and content.
+     * @param array $expectedSymlinks
+     *   List of expected symlinks, as an hash of "source: destination".
+     * @param array $expectedMissingSymlinks
+     *   List of symlinks that shouldn't be created.
+     *
+     * @dataProvider dataProvider
+     */
+    public function testProjectSymlinkCommands(array $configuration, array $project, array $expectedSymlinks, array $expectedMissingSymlinks)
+    {
+        // Create test project content.
+        $fs = new Filesystem();
+        $fs->remove($this->getSandboxRoot());
+        foreach ($project['content']['directories'] as $name) {
+            $fs->mkdir($this->getSandboxRoot().'/'.$name);
+        }
+        foreach ($project['content']['files'] as $name) {
+            $fs->touch($this->getSandboxRoot().'/'.$name);
+        }
+
+        // Create test task runner configuration file.
+        $configFile = $this->getSandboxFilepath('runner.yml');
+        file_put_contents($configFile, Yaml::dump($configuration));
+
+        $input = new StringInput("drupal:symlink-project --working-dir=".$this->getSandboxRoot());
+        $output = new BufferedOutput();
+        $runner = new TaskRunner($input, $output, $this->getClassLoader());
+
+        // Mock composer service by settings test project name and type.
+        $mock = $this->createMock(Composer::class);
+        $mock->method('getProject')->willReturn($project['name']);
+        $mock->method('getType')->willReturn($project['type']);
+        $runner->getContainer()->share('task_runner.composer', $mock);
+
+        // Run command.
+        $runner->run();
+    }
+
+    /**
+     * Test data provider.
+     *
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return Yaml::parse(file_get_contents(__DIR__.'/../fixtures/symlink-project.yml'));
+    }
+
+    /**
+     * Get local test sandbox directory.
+     *
+     * @return string
+     */
+    protected function getSandboxRoot()
+    {
+        return __DIR__."/../sandbox";
+    }
+
+    /**
+     * Get full path of a file in the sandbox directory.
+     *
+     * @param string $name
+     *   File name, relative to the sandbox directory.
+     *
+     * @return string
+     */
+    protected function getSandboxFilepath($name)
+    {
+        return $this->getSandboxRoot().'/'.$name;
+    }
+
+    /**
+     * Get project class loader.
+     *
+     * @return \Composer\Autoload\ClassLoader
+     */
+    protected function getClassLoader()
+    {
+        return require __DIR__.'/../../vendor/autoload.php';
+    }
+}
